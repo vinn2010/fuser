38d9bb2d66a3345b20340ef9927ff6b903d57697
Kevin Deb <vinn@kevinize.com>
2023-10-15 01:16:56 +0700

    error handling for args

--- fuser.c
+++ fuser.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <sys/stat.h>
 
 // the first arg is the number of arguements pasted
 // to the function
+
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define CLEAR "\x1B[0m"
+
+struct stat info;
+
 int main(int argc, char **argv) {
-  printf("%d\n", argc);
-  printf("%s\n", argv[1]);
-  printf("%s\n", argv[2]);
-  printf("%s\n", argv[3]);
-  printf("%s\n", argv[4]);
+
+  if(argc < 2 ) {
+    //fprintf = allows string concat
+    //must has stderr first
+    fprintf(stderr, RED "Error: " CLEAR "provide a file\n" );
+    printf("Usage: fuser <file>\n");
+    return 1;
+  }
+
+  if(argc > 2) {
+    fprintf(stderr, RED "Error: " CLEAR "Too many arguements\n" );
+    printf("Usage: fuser <file>\n");
+    return 1;
+  }
+
+  char *file = argv[1];
+  //  getpwuid(file);
+
+  printf("%s", file);
   return 0;
 }
