4b3efc9db26ecf812730348b3dd284ecfd8ad74f
Kevin Deb <vinn@kevinize.com>
2023-10-16 18:09:54 +0700

    added group

--- fuser.c
+++ fuser.c
@@ -3,6 +3,7 @@
 
 #include <sys/stat.h> // stat(function)
 #include <pwd.h>
+#include <grp.h>
 
 // the first arg is the number of arguements pasted
 // to the function
@@ -43,18 +44,30 @@ int main(int argc, char **argv) {
   // the stat function gets unix info about the file
   // and passes it to the stat struct
   stat(file, &st);
-  // st returns numericl values that represent the user info.
+  // st returns numericl value for user ID for the file
   // eg - st.st_uid = 1001 for vinn
-  // to convert it we need pwd.h
+  // to convert it to get user name we need pwd.h
   // which provides the struct passwd
 
   struct passwd *user = getpwuid(st.st_uid);
   // user is now a struct with decoded user info
 
-  printf("%s", user->pw_name);
   // pw_name is one of the members of the passwd struct
 
   //lets also add the group
+
+  //group struct from grp.h
+  //get group info based on group id of file
+  //which comes from the st struct
+  struct group *gr = getgrgid(st.st_gid);
+
+
+  // output
+  printf("User:Group\n");
+  printf("%s", user->pw_name);
+  printf(":");
+  printf("%s\n", gr->gr_name);
+  
   return 0;
 }
 
